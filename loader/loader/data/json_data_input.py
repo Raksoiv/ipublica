5215d9d7afd8468c6d705d48f03c5dc227383471
Oscar Rencoret <oscar.rencoret@unholster.com>
2020-03-11 12:25:12 -0300

    json input data

---
+++ loader/loader/data/json_data_input.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+from loader.bussiness import InputDataInterface
+from loader import settings
+
+
+class JsonDataInput(InputDataInterface):
+    def __init__(self):
+        self.scraper_data_path = settings.SCRAPER_DATA_PATH
+
+    def list_data_ids(self) -> list:
+        return list(map(
+            lambda name: '.'.join(name.split('.')[:-1]),
+            os.listdir(self.scraper_data_path)
+        ))
+
+    def get_data(self, data_id: str) -> dict:
+        filename = os.path.join(self.scraper_data_path, f'{data_id}.json')
+        with open(filename, 'r') as fi:
+            data = json.loads(fi.read())
+        return data
