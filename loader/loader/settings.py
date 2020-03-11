db60b98532bfa9ad3b764b88332622fafdd20aae
Oscar Rencoret <oscar.rencoret@unholster.com>
2020-03-11 12:28:03 -0300

    added settings file for environment variables

---
+++ loader/loader/settings.py
@@ -0,0 +1,21 @@
+import os.path
+from os import getenv
+
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = getenv('DATA_PATH', os.path.join(BASE_PATH, 'data'))
+SCRAPER_DATA_PATH = getenv(
+    'SCRAPER_DATA_PATH',
+    os.path.join(BASE_PATH, 'scraper_data')
+)
+DATABASE_URL = getenv(
+    'DATABASE_URL',
+    os.path.join(BASE_PATH, 'data', 'loader.db')
+)
+NEO4J_URL = getenv(
+    'NEO4J_URL',
+    'bolt://localhost:7687',
+)
+NEO4J_AUTH = tuple(getenv(
+    'NEO4J_AUTH',
+    'neo4j,unholster'
+).split(','))
