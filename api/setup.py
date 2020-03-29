90cde0027349a5936582ba25963ff4322ba135cc
Oscar Rencoret <oscar.rencoret@unholster.com>
2020-03-29 13:49:23 -0300

    Added basic api for resource benchmark

---
+++ api/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='api',
+    version='0.1',
+    description='Api',
+    author='Oscar Rencoret',
+    author_email='o.rencoret@gmail.com',
+    packages=find_packages(),
+)
